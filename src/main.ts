d0f3d0d1bba5ad90fdd90629821a42847c2f745a
nagarjunveni <v.nagarjun@gmail.com>
2018-05-30 09:55:24 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,38 +9,5 @@ if (environment.production) {
 }
 
 /*
-
-import {UitkExpandCollapseAnimations} from '../../core/utilities/animations';
-animations:[UitkExpandCollapseAnimations.expandOrCollapsed],
-
-[@expandOrCollapsed]="{value:'',params: {duration: tkAnimationTime|| 500}}"
-(@expandOrCollapsed.start)='_toggleOverflow($event)'
-(@expandOrCollapsed.done)='_toggleOverflow($event)'
-                        
-private _toggleOverflow = UitkExpandCollapseAnimations.toggleOverflowHandler;
-
-export const UitkExpandCollapseAnimations: {
-    readonly expandOrCollapsed: AnimationTriggerMetadata,
-    readonly toggleOverflowHandler: (event: AnimationEvent) => void
-} = {
-    expandOrCollapsed: trigger('expandOrCollapsed', [
-        state('void', style({height: '0px', visibility: 'hidden'})),
-        transition(':enter', [
-            animate("{{duration}}ms")]),
-        transition(':leave', [
-            animate("{{duration}}ms")]),
-    ]),
-    toggleOverflowHandler: toggleOverflowHandler
-}
-export function toggleOverflowHandler(event: AnimationEvent) {
-    // console.log(event, 'event');
-    const {phaseName} = event;
-    if (phaseName === 'start') {
-        event.element.style.overflow = 'hidden';
-    }
-    else if (phaseName === 'done') {
-        event.element.style.overflow = 'visible';
-    }
-};
 */
 platformBrowserDynamic().bootstrapModule(AppModule);
